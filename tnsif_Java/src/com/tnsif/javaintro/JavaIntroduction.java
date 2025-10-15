0670ea7ab348bef556cf1827ce93e8495cf9ff1e
Hp <Hp@DESKTOP-F2RSRDI>
2025-10-15 09:11:14 +0530

    Initial Commit

---
+++ tnsif_Java/src/com/tnsif/javaintro/JavaIntroduction.java
@@ -0,0 +1,37 @@
+package com.tnsif.javaintro;
+
+public class JavaIntroduction {
+
+
+		public static void main(String[] args) {
+			
+			//Identifers - age, salary, isPassed & grade
+			
+			int age = 20; //variable or data member
+			double salary = 50000.00;
+			boolean isPassed = true;
+			char grade = 'A';
+			//String name = "ABCD";
+			
+			
+			//Type Casting
+			
+			double newSalary = age; //implicit or internal
+			int roundSalary = (int)salary;//external or external
+			
+			//Arithmetic Operation
+			int bonus = 5000;
+			int total = roundSalary + bonus;
+			
+			
+			System.out.println(age);
+			System.out.println(salary);
+			System.out.println(roundSalary);
+			System.out.println(total);
+			
+
+		}
+
+	}
+
+
